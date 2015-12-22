967cdea1a5e6b7cb47e2d0542a364fc1670d7c8e
EzryStIago <ezrysm@gmail.com>
2015-12-22 18:25:34 -0500

    Added functions as required

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Provides a container for a matrix and its inverse. This allows the inverse
+## to be cached, saving computational time (should the inverse be needed often)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Returns a list of a matrix, its inverse, and functions to set and get each
+makeCacheMatrix <- function(mtrx = matrix()) {
+  inv <- NULL #inverse is, by default, NULL
+  
+  #When an existing cacheMatrix is altered, the inverse is reset to NULL
+  set <- function(y){
+    mtrx <<- y
+    inv <<- NULL
+  }
+  
+  #return the matrix itself
+  get <- function() mtrx
+  
+  #set the inverse
+  setinverse <- function(inverse) inv <<- inverse
+  
+  #return the inverse of the matrix
+  getinverse <- function() inv
+  
+  #return the list of all variables constructed herein
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##Checks if the inverse of the matrix has already been calculated.
+##If so, it returns the cached inverse.
+##If not, it calculates and stores the inverse as the inv variable
+##of the cacheMatrix.
+cacheSolve <- function(cmtrx, ...) {
+  inv <- cmtrx$getinverse() #get the cached inverse, if present
+  
+  #if the cached inverse is not present (inv is null), 
+  #then calculate and cache the inverse.
+  if(is.null(inv)){
+    message("calculating...")
+    data <- cmtrx$get()
+    inv <- solve(data)
+    cmtrx$setinverse(inv)
+  }
+  
+  inv
 }
